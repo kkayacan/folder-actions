201a89e48b64f319c6f7886e9d5e07b244e1a744
Kerem Kayacan <kerem.kayacan@acron.corp>
2019-04-14 14:54:47 +0300

    Test script

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+from os import scandir
+
+def convert_date(timestamp):
+    d = datetime.utcfromtimestamp(timestamp)
+    formated_date = d.strftime('%d %b %Y')
+    return formated_date
+
+def get_files():
+    dir_entries = scandir('D:\\home\\tmp')
+    for entry in dir_entries:
+        if entry.is_file():
+            info = entry.stat()
+            print(f'{entry.name}\t Last Modified: {convert_date(info.st_mtime)}')
+
+get_files()
